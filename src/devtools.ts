d6a3d4461ebfb6633123cbd2f3b5d5bb8738704f
Felix Fatka <felix.fatka@gmail.com>
2022-02-15 15:51:18 +0100

    feat(devtools): added to chrome devtools

--- src/devtools.ts
+++ src/devtools.ts
@@ -2,13 +2,5 @@ chrome.devtools.panels.create("Angular Inspector",
   "icon.png",
   "inspector.html",
   function(panel) {
-
   }
 );
-
-
-chrome.devtools.panels.elements.createSidebarPane("My Sidebar",
-  function(sidebar) {
-    // sidebar initialization code here
-    sidebar.setObject({ some_data: "Some data to show" });
-  });
